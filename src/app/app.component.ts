7017075c89caac3234d7a1c37575d9f5af7210e5
David Thong Nguyen <thnguyen2808@gmail.com>
2018-08-07 16:02:05 +0700

    Finished first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import * as $ from 'jquery';
+import * as screenshooter from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   pillShapeText = '';
   pillShapesArray: string[] = [];
   pillShapeColor = 'salmon';
 
+  ngOnInit(): void {
+  }
+
   parsePillInput() {
     this.pillShapesArray = this.pillShapeText.trim().split(',');
+  }
+
+  screenshot() {
+    // var test1=imagify(d.body,(base64)=>{
+    //   $('body').append('<img src="'+base64+'"/>')
+    // });
+    //
+    screenshooter($('#pill-shape-gen-output')[0]).then(function (canvas) {
+      document.body.appendChild(canvas);
+    });
   }
 }
